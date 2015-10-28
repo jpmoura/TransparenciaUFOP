b52bb5b96e072032b3ed131f79aeadc6e9f30916
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2015-10-28 02:02:13 -0200

    Commit inicial

---
+++ codigo.java
@@ -0,0 +1,13 @@
+/* Exemplo de um fragmento de codigo em Java */
+
+public class Main {
+	int variavel;
+	
+	public Main() {
+		System.out.println("Construtor");
+	}
+	
+	public void static main(args[]) {
+		System.out.println("Hello World!")
+	}
+}
